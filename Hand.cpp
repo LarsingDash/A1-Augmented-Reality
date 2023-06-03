b3a7395aaa60ca60851b7b067eadc821148b67a0
Owen Verhoeven <owenverhoeven@protonmail.com>
2023-06-03 15:02:05 +0200

    Mouse Click

--- Hand.cpp
+++ Hand.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "hand.h"
+#include <Windows.h>
+#include <opencv2/opencv.hpp>
 
 // Constants for cursor control
 const int SCREEN_WIDTH = GetSystemMetrics(SM_CXSCREEN);
@@ -9,58 +10,37 @@ int lastY;
 
 // Function to move the cursor
 void moveCursor(int x, int y) {
-    int newX = SCREEN_WIDTH - ((x * SCREEN_WIDTH) / 607);  // Reverse and map the x-coordinate to the screen width
-    int newY = (y * SCREEN_HEIGHT) / 342;  // Mapping the y-coordinate to the screen height
+    int newX = SCREEN_WIDTH - ((x * SCREEN_WIDTH) / 607);  // Map the x-coordinate to the screen width
+    int newY = (y * SCREEN_HEIGHT) / 342;  // Map the y-coordinate to the screen height
 
-    if (lastY > newY)
-    {
-        newY /= 2;
-    } else if(lastY < newY)
-    {
-        newY *= 2;
-    }
+    newX = std::max(0, std::min(newX, SCREEN_WIDTH));
+    newY = std::max(0, std::min(newY, SCREEN_HEIGHT));
 
-    if (lastX > newX)
-    {
-        newX /= 2;
-    }
-    else if (lastX < newX)
-    {
-        newX *= 2;
-    }
-
-	if(newX > SCREEN_WIDTH)
-	{
-		newX = SCREEN_WIDTH;
-	}
-
-	if(newX < 0)
-	{
-		newX = 0;
-	}
-
-	if (newY > SCREEN_HEIGHT)
-	{
-		newY = SCREEN_HEIGHT;
-	}
-
-	if (newY < 0)
-	{
-		newY = 0;
-	}
-
-    lastX = x;
-    lastY = y;
+    lastX = newX;
+    lastY = newY;
 
     SetCursorPos(newX, newY);
-    
 }
 
+// Function to simulate a mouse button press
+void mouseButtonDown() {
+    INPUT input;
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void mouseButtonUp() {
+    INPUT input;
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(1, &input, sizeof(INPUT));
+}
 
 int hand() {
     cv::VideoCapture cap(1);
     cap.set(cv::CAP_PROP_FRAME_WIDTH, 607); // valueX = your wanted width
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 342); // valueY = your wanted heigth
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 342); // valueY = your wanted height
 
     if (!cap.isOpened()) {
         std::cerr << "Failed to open camera." << std::endl;
@@ -75,7 +55,8 @@ int hand() {
     if (handcascade_palm.empty()) {
         std::cerr << "Failed to load hand cascade file." << std::endl;
         return -1;
-    }  if (handcascade_fist.empty()) {
+    }
+    if (handcascade_fist.empty()) {
         std::cerr << "Failed to load hand cascade file." << std::endl;
         return -1;
     }
@@ -106,8 +87,10 @@ int hand() {
             cv::putText(frame, "Open", cv::Point(hand.x, hand.y - 10), cv::FONT_HERSHEY_SIMPLEX, 0.9, cv::Scalar(255, 0, 255), 2);
 
             // Move the cursor based on hand position
-            moveCursor(center.x, center.y);
-        }for (const auto& hand : fist) {
+            moveCursor(hand.x + hand.width / 2, hand.y + hand.height / 2);
+            // mouseButtonUp();
+        }
+        for (const auto& hand : fist) {
             cv::Point center(hand.x + hand.width / 2, hand.y + hand.height / 2);
             cv::circle(frame, center, hand.width / 2, cv::Scalar(255, 0, 255), 2);
 
@@ -116,7 +99,8 @@ int hand() {
             cv::putText(frame, "Closed", cv::Point(hand.x, hand.y - 10), cv::FONT_HERSHEY_SIMPLEX, 0.9, cv::Scalar(255, 0, 255), 2);
 
             // Move the cursor based on hand position
-            moveCursor(center.x, center.y);
+            moveCursor(hand.x + hand.width / 2, hand.y + hand.height / 2);
+            // mouseButtonDown();
         }
 
         cv::imshow("Hand Recognition", frame);
