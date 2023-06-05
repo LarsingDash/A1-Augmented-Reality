b889e048fcd6362201b0cbd22b21231d0a8cc5e5
koen <kvoostrom011@gmail.com>
2023-06-06 01:28:26 +0200

    small changes

--- main.cpp
+++ main.cpp
@@ -142,33 +142,31 @@ void check_keys()
 		cube_position.z += 0.1;
 	if (glfwGetKey(window, GLFW_KEY_B))
 		cube_position.z -= 0.1;
-}
-void Draw()
-{
-	tigl::shader->setModelMatrix(glm::mat4(1.0f));
-	//Clear
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	rotate = glm::mat4(1.0f); // Initialize the transformation matrix
 	
-	check_keys();
-
 	if (!glfwGetKey(window, GLFW_KEY_X) && !glfwGetKey(window, GLFW_KEY_Y) && !glfwGetKey(window, GLFW_KEY_Z))
 	{
 		angle_x = 0;
 		angle_y = 0;
 		angle_z = 0;
 	}
+}
+void reset_rotations() 
+{
+	rotate = glm::mat4(1.0f); 
+}
+void Draw()
+{
+	tigl::shader->setModelMatrix(glm::mat4(1.0f));
+	//Clear
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
-
-
+	reset_rotations();
+	check_keys();
 
 	tigl::shader->setModelMatrix(glm::translate(glm::mat4(1.f), glm::vec3(cube_position)));
 	rotate = glm::rotate(rotate, glm::radians(angle_z), glm::vec3(0.f, 0.f, 1.f));
 	rotate = glm::rotate(rotate, glm::radians(angle_y), glm::vec3(0.f, 1.f, 0.f));
 	rotate = glm::rotate(rotate, glm::radians(angle_x), glm::vec3(1.f, 0.f, 0.f));
-	
-	
-	
 
 	for (const GameObject& gameObject : gameObjects)
 	{
